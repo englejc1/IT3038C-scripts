a87b5b4ccb51b73a8ec133b1290a5b8c2dacd60b
englejc1 <englejc@mail.uc.edu>
2021-10-10 21:42:02 -0400

    web sever script for lab 6

--- node/server.js
+++ node/server.js
@@ -3,6 +3,19 @@ var http = require("http");
 var ip = require('ip');
 var os = require("os");
 
+function getUptime() {
+    serverTime = os.uptime();
+    var d = Math.floor(serverTime / (3600*24));
+    var h = Math.floor(serverTime % (3600*24) / 3600);
+    var m = Math.floor(serverTime % 3600 / 60);
+    var s = Math.floor(serverTime % 60);
+    var dreturn = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+    var hreturn = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    var mreturn = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+    var sreturn = s > 0 ? s + (s == 1 ? " second, " : " seconds, ") : "";
+    return dreturn + hreturn + mreturn + sreturn;
+}
+
 var server = http.createServer(function(req, res){
     if (req.url === "/") {
         fs.readFile("./public/index.html", "UTF-8", function(err, body){
@@ -11,7 +24,10 @@ var server = http.createServer(function(req, res){
         });            
     }
     else if(req.url.match("/sysinfo")) {
-        myHostName=os.hostname();
+        myHostName = os.hostname();
+        totalMemory = os.totalmem();
+        freeMemory = os.freemem();
+        myCPUs = os.cpus().length;
         html = `
         <!DOCTYPE html>
             <html>
@@ -21,10 +37,10 @@ var server = http.createServer(function(req, res){
                     <body>
                         <p>Hostname: ${myHostName}</P>
                         <p>IP: ${ip.address()}</p>
-                        <p>Server Uptime: </p>
-                        <p>Total Memory: </p>
-                        <p>Free Memory: </p>
-                        <p>Number of CPUs: </p>
+                        <p>Server Uptime: ${getUptime()}</p>
+                        <p>Total Memory: ${totalMemory / 1000000} MB</p>
+                        <p>Free Memory: ${(freeMemory / 1000000)} MB</p>
+                        <p>Number of CPUs: ${myCPUs}</p>
                     </body>
             </html>
         `
