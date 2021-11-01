d5ae89d4643d2de190e2a88db4eb3a620401775e
englejc1 <englejc@mail.uc.edu>
2021-10-31 20:52:54 -0400

    lab 7

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,13 +1,10 @@
 ﻿function getIP{
-
     (Get-NetIPAddress).ipv4address | Select-String "192*"
-
 }
 
 Write-Host(getIP)
-
 $IP = getIP
+$Date = ""
+$Body = "This machine's IP is $IP. User is $env:username. Hostname is $. PowerShell Version . Today's date is $Date"
 
-Write-Host("This machine's IP is $IP")
-
-Write-Host("This machine's IP is {0}" -f $IP)
+write-host($Body)
